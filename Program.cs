c7c1ccd5fc6e9f387619601bc7529bf2b7d1f856
Yana <YanaDrey@rambler.ru>
2023-03-12 17:34:34 +0300

    CountLessFourSimbolsElements

--- Program.cs
+++ Program.cs
@@ -13,5 +13,27 @@ void PrintArray(string[] array)
     System.Console.WriteLine("[" + string.Join(",", array) + "]");
 }
 
+int CountLessFourSimbolsElements(string[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+            result++;
+    return result;
+}
+
+// string[] LessFourSimbolsArray(string[] array)
+// {
+//     string[] newArray = new string[];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+
+//     }
+// }
+
 string[] myArray = InputArray("Введите строки через запятую и пробел: ");
 PrintArray(myArray);
+
+int sizeNewArray = CountLessFourSimbolsElements(myArray);
+System.Console.WriteLine(sizeNewArray);
