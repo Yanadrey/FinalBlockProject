57f4661374a5afca64e9c5c7ff4aa83ea89fbd6b
Yana <YanaDrey@rambler.ru>
2023-03-12 17:20:00 +0300

    Input and Print array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив
+// из строк, длина которых меньше либо равна три символа.
+
+string[] InputArray(string text)
+{
+    System.Console.Write(text);
+    var array = Array.ConvertAll(Console.ReadLine().Split(", "), Convert.ToString);
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.WriteLine("[" + string.Join(",", array) + "]");
+}
+
+string[] myArray = InputArray("Введите строки через запятую и пробел: ");
+PrintArray(myArray);
